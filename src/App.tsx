3d5aacc69ebdb6cb6fd8e1d65c929221a85f15a9
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-09-18 18:57:59 +0545

    feat: App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import ExpertDoctorsPage from "./features/MeetExpertDoctors/Pages/ExpertDoctorsPage"
+import PatientTestimonialsPage from "./features/PatientTestimonials/Pages/PatientTestimonialsPage"
+import Footer from "./features/Footer/Pages/Footer"
 function App() {
 
   return (
     <>
-         </>
+      <ExpertDoctorsPage/>
+      <PatientTestimonialsPage/>
+      <Footer />
+    </>
   )
 }
 
