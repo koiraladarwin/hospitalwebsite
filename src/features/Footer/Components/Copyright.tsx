b3618c05881947faf702489437ad3d723829dff7
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-09-18 18:54:48 +0545

    feat: Footer and copyright section

---
+++ src/features/Footer/Components/Copyright.tsx
@@ -0,0 +1,9 @@
+export default function Copyright()
+{
+    return(
+        <div className="flex justify-between p-2 pl-30 pr-10 bg-copyright-bg text-white ">
+            <h2>Copyright 2025 Sushwastha Hospital. All Rights Reserved</h2>
+            <h2>Powered by <span className="text-blue-300">OCS</span></h2>
+        </div>
+    )
+}
