a50efd708d71526a6916999161632c5b3bcb7567
Aarona KC <aaronakc99@gmail.com>
2025-09-18 15:18:49 +0545

    feat:add the login and serach on nav

--- src/features/home/components/nav/Nav.tsx
+++ src/features/home/components/nav/Nav.tsx
@@ -1,22 +1,28 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../../../../assets/logo.svg';
 import { RiArrowDropDownLine } from 'react-icons/ri';
+import { FaSearch } from 'react-icons/fa';
 function NavBar() {
   return (
-    <div className="bg-white h-70 md:h-25 px-10 pt-2 shadow-lg flex flex-col lg:flex-row items-center justify-between md:px-15 md:pb-3 lg:py-0 lg:h-18 sticky top-0 z-2 ">
+    <div className="bg-white h-70 md:h-28 px-10 pt-2 shadow-lg flex flex-col xl:flex-row items-center justify-between md:px-15 md:pb-3 xl:py-0 xl:h-18 sticky top-0 z-2 ">
 
       <div className='flex items-center pt-2 pb-3 md:pt-0 md:pb-0'>
         <img src={logo} className='w-60' />
       </div>
 
-      <div className='flex flex-col justify-center pb-10 md:flex-row  items-center gap-6 md:pb-0'>
-        <ul className='flex flex-col gap-3 md:flex-row md:gap-12 font-poppins text-gray '>
+      <div className='flex flex-col justify-center pb-5 md:flex-row  items-center gap-4 md:gap-8 md:pb-0'>
+        <ul className='flex flex-col gap-3 md:flex-row lg:gap-12 md:gap-4 font-poppins text-gray '>
           <li><NavLink to='/' className={'hover:text-gray-light'}>About</NavLink></li>
           <li><NavLink to='/' className={'flex items-center hover:text-gray-light'}>Therapy <RiArrowDropDownLine size={20} /></NavLink></li>
           <li><NavLink to='/' className={'hover:text-gray-light'}>Our Focus</NavLink></li>
           <li><NavLink to='/' className={'hover:text-gray-light'}>Contact</NavLink></li>
         </ul>
-        <button className='bg-green text-white px-6 py-1 font-poppins rounded-md hover:bg-green-light'>Book now</button>
+
+        <div className='flex gap-4 items-center'>
+          <button className='bg-green text-white px-6 py-1 font-poppins rounded-2xl hover:bg-green-light'>Book now</button>
+          <button className='bg-orange-gradient text-white px-6 py-1 font-poppins rounded-2xl hover:bg-orange-200'>Login</button>
+          <FaSearch color='gray' />
+        </div>
       </div>
 
     </div>
