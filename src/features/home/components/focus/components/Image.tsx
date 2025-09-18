ed557f0ccbe4797683e7e3dd7c38f41085f9c237
Aarona KC <aaronakc99@gmail.com>
2025-09-18 22:12:32 +0545

    feat:add the text on image on therapy

--- src/features/home/components/focus/components/Image.tsx
+++ src/features/home/components/focus/components/Image.tsx
@@ -6,23 +6,46 @@ import acupressure from '../../../../../assets/acupressure.jpeg'
 function Image() {
   return (
     <div className='flex gap-4'>
-      <div className="flex flex-col gap-4 pt-15">
-        <img
-          src={hotstone}
-          className={`w-40 h-40 rounded-md object-cover`} />
-        <img
-          src={manipulation}
-          className={`w-40 h-40 rounded-md object-cover`} />
+      <div className="flex flex-col gap-4 pt-15 ">
+        <div className="relative w-40 h-40 rounded-md overflow-hidden">
+          <img
+            src={hotstone}
+            className={`w-full h-full rounded-md object-cover`} />
+          <p className="absolute w-full top-30 text-center text-black font-bold bg-transparent-white bg-opacity-50 px-1 rounded font-poppins">
+            Hot Stone
+          </p>
+        </div>
 
+        <div className="relative w-40 h-40 rounded-md overflow-hidden">
+          <img
+            src={manipulation}
+            className={`w-40 h-40 rounded-md object-cover`} />
+          <p className="absolute w-full top-30 text-center text-black font-bold bg-transparent-white bg-opacity-50 px-1 rounded font-poppins">
+            Manipulation
+          </p>
+        </div>
       </div>
 
       <div className="flex flex-col gap-4 pt-5">
-        <img
-          src={exercise}
-          className={`w-40 h-40 rounded-md object-cover`} />
-        <img
-          src={acupressure}
-          className={`w-40 h-40 rounded-md object-cover`} />
+        <div className="relative w-40 h-40 rounded-md overflow-hidden">
+          <img
+            src={exercise}
+            className={`w-40 h-40 rounded-md object-cover`} />
+          <p className="absolute w-full top-30 text-center text-black font-bold bg-transparent-white bg-opacity-50 px-1 rounded font-poppins">
+            Exercise
+          </p>
+        </div>
+
+
+        <div className="relative w-40 h-40 rounded-md overflow-hidden">
+          <img
+            src={acupressure}
+            className={`w-40 h-40 rounded-md object-cover`} />
+          <p className="absolute w-full top-30 text-center text-black font-bold bg-transparent-white bg-opacity-50 px-1 rounded font-poppins">
+            Acupressure
+          </p>
+        </div>
+
       </div>
     </div>
   )
