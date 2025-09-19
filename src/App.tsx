5b88473de3bd13daa53528073d992b1313284b21
Darwin Koirala <darwinkoirala123@gmail.com>
2025-09-19 10:06:26 +0545

    chore: added react icons and swipe dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import ExpertDoctorsPage from "./features/MeetExpertDoctors/Pages/ExpertDoctorsPage"
 import PatientTestimonialsPage from "./features/PatientTestimonials/Pages/PatientTestimonialsPage"
 import Footer from "./features/Footer/Pages/Footer"
+import ExpertDoctorsPage from "./features/MeetExpertDoctors/pages/ExpertDoctorsPage"
 function App() {
 
   return (
