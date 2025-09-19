6a9b23613eb6c8857c061bb04efae4b4add6f149
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-09-19 15:56:40 +0545

    fix: fixed responsive slider

--- src/features/PatientTestimonials/Components/TestimonialsSlider.tsx
+++ src/features/PatientTestimonials/Components/TestimonialsSlider.tsx
@@ -4,38 +4,42 @@ import "swiper/css/pagination";
 import {  Pagination } from "swiper/modules";
 
 import TestimonialsCard from './TestimonialsCard';
+import { TestimonialsDetails } from './TestimonialsDetails';
+
 export default function ExpertDoctorSlider()
 {
     return(
         <>
-            <div>
+            <div className='px-5 md:px-20 lg:px-32'>
             <Swiper
                 modules={[Pagination]}
-                spaceBetween={50}
-                slidesPerView={1}
+                spaceBetween={1}
                 pagination={{clickable: true, dynamicBullets:true}}
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
                 simulateTouch={true}
                 speed={2000}
+                breakpoints={{
+                    0: {slidesPerView: 1},
+                    768: {slidesPerView: 2},
+                    900:{slidesPerView: 2},
+                    1024: {slidesPerView: 2},
+                    1200: {slidesPerView: 3},
 
+                }}
+                className="pb-10"
             >
-                <SwiperSlide >
-                    <TestimonialsCard/>
-                </SwiperSlide >
-                <SwiperSlide ><TestimonialsCard/></SwiperSlide>
-                <SwiperSlide ><TestimonialsCard/></SwiperSlide>
-                <SwiperSlide ><TestimonialsCard/></SwiperSlide>
-                <SwiperSlide ><TestimonialsCard/></SwiperSlide>
-                <SwiperSlide ><TestimonialsCard/></SwiperSlide>
-                <SwiperSlide ><TestimonialsCard/></SwiperSlide>
+
+                {
+                    TestimonialsDetails.map((testcard,i) => (
+                        <SwiperSlide key={i}>
+                            <TestimonialsCard {...testcard}/>
+                        </SwiperSlide>
+                    ))
+                }
                 
             </Swiper>
             </div>
         </>
     )
 }
-
-
-
- 
