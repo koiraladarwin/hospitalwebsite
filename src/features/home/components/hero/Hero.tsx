19c8c92d7230ff115283a8ff778813bd1b6aadcd
Aarona KC <aaronakc99@gmail.com>
2025-09-19 22:06:53 +0545

    fix:change the hero ui

--- src/features/home/components/hero/Hero.tsx
+++ src/features/home/components/hero/Hero.tsx
@@ -1,35 +1,27 @@
-import heroBg from '../../../../assets/heroBg.png'
-import Button from '../button/Button'
+import heroBg from '../../../../assets/heroBg.png';
+import AppointmentForm from './components/AppointmentForm';
+
 function Hero() {
   return (
     <div
-      className="w-full h-[50vh] md:h-[100vh] bg-cover bg-center relative"
+      className="w-full h-[45vh] md:h-[calc(115vh-15vh)] bg-cover bg-center relative overflow-visible"
       style={{ backgroundImage: `url(${heroBg})` }}
     >
-      <div className='flex flex-col absolute  top-15 left-10 [@media(min-width:1152px)]:left-45  lg:top-20 md:left-5 md:top-20 lg:left-20  gap-4'>
-        <div>
-          <p className='text-green-medium  text-sm font-poppins font-extrabold lg:text-5xl md:text-4xl md:leading-13'>A holistic<br />
-            approach to <br />
-            balance the body, <br />
-            mind,and soul.
-          </p>
-        </div>
 
-        <div className="pt-0">
-          <p className="font-poppins text-[8px] md:text-base text-gray-700 leading-snug md:leading-relaxed">
-            Providing harmless and non-invasive natural therapies<br />
-            aiming to treat each individual health
-          </p>
-        </div>
+      <div className='flex flex-col absolute top-15 left-10 [@media(min-width:1152px)]:left-35 lg:top-30 md:left-5 md:top-30 lg:left-20 gap-4'>
+        <p className='text-sm font-[Akriti-Regular] font-extrabold lg:text-5xl md:text-4xl md:leading-18'>
+          <span className='text-orange-bg'>कमर वा गर्दन <br /></span>
+          <span className='text-green pl-17'>दुख्नु भनेको के हो ?</span>
+        </p>
+      </div>
 
-        <div className='flex gap-2  md:flex-row md:gap-6 pt-2'>
-          <Button title="Book Consultation" bgColor='bg-orange-gradient' textColor='text-white' hoverColor='hover:bg-orange-300' />
-          <Button title="Our services" bgColor='bg-white' textColor='text-black' hoverColor='hover:bg-slate-100' />
+      <div className="absolute top-[120px] md:top-[465px] w-full px-4 md:px-10 lg:px-20 flex justify-center md:justify-end">
+        <div className="w-full max-w-[1600px]">
+          <AppointmentForm />
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
