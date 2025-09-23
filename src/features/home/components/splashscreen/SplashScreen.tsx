30d4dfce8bf489cee8794b4e288eb2dc1edf83e9
Krishma Maharjan <126758112+krishmamaharjan@users.noreply.github.com>
2025-09-23 17:02:27 +0545

    Feat/logoanimation (#14)
    * chore: animation added in logo
    
    * chore: logo animation added
    
    * chore: logo animation
    
    * feat: motion added in package
    
    ---------
    
    Co-authored-by: Darwin <darwinkoirala123@gmail.com>

--- src/features/home/components/splashscreen/SplashScreen.tsx
+++ src/features/home/components/splashscreen/SplashScreen.tsx
@@ -1,18 +1,30 @@
+import {motion} from "framer-motion";
 import logo2 from "../../../../assets/logo2.svg";
 
 export const SplashScreen = () => {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-white text-center gap-10">
-      <p className="font-[Akriti-Regular] text-[#73a91b] text-xl md:text-4xl font-semibold mb-4">
-        फिजियोथेरेपी, अकुपञ्चर, प्राकृतिक चिकित्सा
-      </p>
-      <img
-        src={logo2}
-        alt="Logo"
-        className="max-w-[70%] max-h-[40%] w-auto h-auto select-none pointer-events-none"
-        draggable={false}
-      />
-    </div>
+    <motion.div>
+
+      <div className="h-screen w-screen flex flex-col items-center justify-center bg-white text-center gap-10">
+
+          <p className="font-[Akriti-Regular] text-[#73a91b] text-xl md:text-4xl font-semibold mb-4">
+            फिजियोथेरेपी, अकुपञ्चर, प्राकृतिक चिकित्सा
+          </p>
+          <motion.img
+            src={logo2}
+            alt="Logo"
+            layoutId="App-logo"
+           
+            transition={{ duration:0.08, ease: "easeIn"}}
+            
+            className="max-w-[70%] max-h-[40%] w-auto h-auto select-none pointer-events-none"
+            draggable={false}
+          />
+        
+      </div>
+
+      
+    </motion.div>
   );
 };
 
--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -8,9 +8,15 @@ import Hero from "../components/hero/Hero";
 import NavBar from "../components/nav/Nav";
 import Therapy from "../components/therapy/Therapy";
 import { SplashScreen } from "../components/splashscreen/SplashScreen";
+import { AnimatePresence } from "framer-motion";
+import {motion} from "framer-motion";
+
+import logo2 from "../../../assets/logo2.svg";
 
 function Home() {
   const [hasInteracted, setHasInteracted] = useState(false);
+
+  const [showNavbar, setShowNavbar] = useState(false);
 
   useEffect(() => {
     const handleUserScroll = () => {
@@ -29,37 +35,61 @@ function Home() {
     };
   }, []);
 
-  if (!hasInteracted) {
-    return (<SplashScreen />);
-  }
 
   return (
-    <div className="w-full min-h-screen bg-main-bg">
-      <NavBar />
-      <div className="overflow-x-hidden">
-        <section id="home">
-          <Hero />
-        </section>
-        <section id="focus">
-          <Focus />
-        </section>
-        <section id="therapy">
-          <Therapy />
-        </section>
+    <div className="w-full min-h-screen bg-main-bg relative">
 
-        <section id="doctors">
-          <ExpertDoctorsPage />
-        </section>
+      <AnimatePresence mode="wait">
+        {!hasInteracted && <SplashScreen key="splash" />}
+      </AnimatePresence>
 
-        <section id="testimonials">
-          <PatientTestimonialsPage />
-        </section>
+      {hasInteracted && !showNavbar && (
+        <motion.div
+          className="absolute top-0 left-0 w-full flex justify-start px-10 pt-3 z-50 opacity-50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          onAnimationComplete={() => setShowNavbar(true)}
+        >
+          <motion.img
+            src={logo2} 
+            layoutId="App-logo"
+            className="w-60"
+            transition={{ duration: 0.4, ease: "easeIn" }}
+            
+          />
+        </motion.div>
+      )}
 
-        <section id="footer">
-        <Footer />
-        </section>
+      {showNavbar && <NavBar />}
 
-      </div>
+      {hasInteracted && (
+         <div className="overflow-x-hidden">
+          
+
+         <section id="home">
+           <Hero />
+         </section>
+         <section id="focus">
+           <Focus />
+         </section>
+         <section id="therapy">
+           <Therapy />
+         </section>
+
+         <section id="doctors">
+           <ExpertDoctorsPage />
+         </section>
+
+         <section id="testimonials">
+           <PatientTestimonialsPage />
+         </section>
+
+         <section id="footer">
+         <Footer />
+         </section>
+
+       </div>
+      )}
     </div>
   );
 }
