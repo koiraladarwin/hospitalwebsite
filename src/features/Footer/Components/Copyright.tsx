96b7f7ba88de6bec5af78d75bcda654576917643
darwin koirala <darwinkoirala123@gmail.com>
2025-09-23 11:54:21 +0545

    chore: made out focus semi bold

--- src/features/Footer/Components/Copyright.tsx
+++ src/features/Footer/Components/Copyright.tsx
@@ -1,9 +1,9 @@
 export default function Copyright()
 {
     return(
-        <div className="flex justify-between p-2 px-5 bg-copyright-bg text-white ">
-            <h2>Copyright 2025 Sushwastha Hospital. All Rights Reserved</h2>
-            <h2>Powered by <span className="text-blue-300">OCS</span></h2>
+        <div className="flex justify-between p-1 px-6 bg-copyright-bg text-white ">
+            <h4>Copyright 2025 Sushwastha Hospital. All Rights Reserved</h4>
+            <h4>Powered by <span className="text-blue-300">OCS</span></h4>
         </div>
     )
 }
