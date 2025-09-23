cf5eb1f4c48a027b722b46e6a9780cdbde2c543d
darwin koirala <darwinkoirala123@gmail.com>
2025-09-23 12:53:04 +0545

    fix: changed footer

--- src/features/Footer/Components/Copyright.tsx
+++ src/features/Footer/Components/Copyright.tsx
@@ -1,7 +1,7 @@
 export default function Copyright()
 {
     return(
-        <div className="flex justify-between p-1 px-6 bg-copyright-bg text-white ">
+        <div className="flex justify-between p-1 px-6 bg-copyright-bg text-white text-sm">
             <span>Copyright 2025 Sushwastha Hospital. All Rights Reserved</span>
             <span>Powered by <span className="text-blue-300">OCS</span></span>
         </div>
