6d06618fbf0a56beabcfbcefa3fad3c786e5de39
Aarona KC <aaronakc99@gmail.com>
2025-09-23 16:23:04 +0545

    change the form position and form design

--- src/features/home/components/hero/Hero.tsx
+++ src/features/home/components/hero/Hero.tsx
@@ -4,33 +4,35 @@ import AppointmentForm from './components/AppointmentForm';
 
 function Hero() {
   return (
-    <div className="w-full md:min-h-[calc(100vh-6rem)] bg-cover bg-center flex flex-col px-4 md:px-10 lg:px-20 py-6">
+    <div className="w-full md:min-h-[calc(100vh-6rem)] bg-cover bg-center flex flex-col px-4 md:px-10 lg:px-20 py-0">
 
       {/* Top section: text + image */}
-      <div className="flex flex-col-reverse md:flex-row items-end justify-end md:items-center  md:justify-around">
+      <div className="flex flex-col-reverse md:flex-row items-end justify-end ps-20 md:items-center  md:justify-around  md:gap-5 ">
 
         {/* Text */}
-        <div className="w-full md:w-1/2 text-center md:text-left flex justify-center md:justify-end items-end">
-          <p className="text-2xl  font-extrabold md:text-4xl lg:text-5xl xl:text-7xl 3xl:text-8xl leading-snug md:leading-[5rem] 2xl:leading-[6rem]">
-            <span className="text-orange-bg font-[Akriti-Regular]">कमर वा गर्दन <br /></span>
-            <span className="text-green pl-6 md:pl-12 font-[Akriti-Regular]">दुख्नु भनेको के हो </span>
-            <span className='text-green '>?</span>
-          </p>
+        <div className=' flex flex-col '>
+          <div className="w-full text-center md:text-left flex justify-center md:justify-start items-start pb-2">
+            <p className="text-2xl  font-extrabold md:text-4xl lg:text-5xl xl:text-7xl 3xl:text-8xl leading-snug md:leading-[5rem] 2xl:leading-[6rem]">
+              <span className="text-orange-bg font-[Akriti-Regular]">कमर वा गर्दन <br /></span>
+              <span className="text-green pl-6 md:pl-12 font-[Akriti-Regular]">दुख्नु भनेको के हो </span>
+              <span className='text-green '>?</span>
+            </p>
+          </div>
+
+          <div className="w-full max-w-[1600px]  flex justify-center md:pt-0  ">
+            <AppointmentForm />
+          </div>
+
         </div>
 
         {/* Image */}
-        <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+        <div className="w-full md:w-4/5 flex justify-center md:justify-end  items-end ">
           <img
             src={heroBg}
             alt="Hero"
             className="h-[350px] w-auto  md:h-[450px] lg:h-[600px] object-contain"
           />
         </div>
-      </div>
-
-      {/* Appointment Form */}
-      <div className=" w-full max-w-[1600px] mx-auto flex justify-center px-10 lg:px-41">
-        <AppointmentForm />
       </div>
     </div>
   );
--- src/features/home/components/hero/components/AppointmentForm.tsx
+++ src/features/home/components/hero/components/AppointmentForm.tsx
@@ -2,44 +2,56 @@ import { BsCalendar2EventFill } from "react-icons/bs";
 
 function AppointmentForm() {
   return (
-    <div className="flex flex-col md:flex-row md:w-full font-poppins">
-      <div className="flex w-full md:w-auto bg-orange-bg justify-center items-center flex-1 md:px-3 px-1 py-1 md:py-0 ">
-        <div className="flex flex-col gap-1 items-center justify-center">
-          <BsCalendar2EventFill color='white' size={24} />
-          <p className="text-center text-white whitespace-nowrap text-sm hidden md:block font-poppins">Make an Appoinment</p>
+    <div className="flex flex-col md:w-full font-poppins border-2 bg-white px-2 md:py-2 py-2   border-dark-green rounded-lg shadow-lg">
+
+      <div className="bg-white justify-center items-center flex-1 px-3 md:flex">
+        <h4 className="text-dark-green text-center text-md font-bold font-poppins">MAKE AN APPOINTMENT</h4>
+      </div>
+
+      <div className="flex  md:flex-row flex-2 md:px-5 px-2 py-2 justify-center md:gap-4 gap-1 items-center bg-white w-full  pb-5">
+        <div className="flex flex-col md:gap-3 gap-1">
+          <div>
+            <label className="font-medium font-poppins text-[0.625rem] md:text-[0.75rem] "> Name</label>
+            <input type="text" className="border-1 border-gray-300 placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px] rounded shadow-md bg-white" placeholder="Name*" />
+          </div>
+          <div>
+            <label className="font-medium font-poppins text-[0.625rem] md:text-[0.75rem] ">Appointment Date</label>
+            <input type="text" className="border-1 border-gray-300 placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px] rounded shadow-md" placeholder="Appointment Date* " />
+          </div>
+        </div>
+
+        <div className="flex flex-col md:gap-3 gap-1">
+          <div>
+            <label className="font-medium font-poppins text-[0.625rem] md:text-[0.75rem] ">Mobile Number</label>
+            <input type="text" className="border-1 border-gray-300 placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px] rounded shadow-md bg-white" placeholder="Mobile*" />
+          </div>
+          <div>
+            <label className="font-medium font-poppins text-[0.625rem] md:text-[0.75rem] ">Appointment Time</label>
+            <input type="text" className="border-1 border-gray-300 placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px] rounded shadow-md bg-white" placeholder="Appointment Time*" />
+          </div>
+        </div>
+
+        <div className="flex flex-col md:gap-3 gap-1">
+          <div>
+            <label className="font-medium font-poppins text-[0.625rem] md:text-[0.75rem] ">Email</label>
+            <input type="text" className="border-1 border-gray-300 placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px] rounded shadow-md bg-white" placeholder="Email*" />
+          </div>
+
+          <div>
+            <label className="font-medium font-poppins text-[0.625rem] md:text-[0.75rem] ">Hospital Id</label>
+            <input
+              type="text"
+              className="border-1 border-gray-300 placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px] rounded-md bg-white shadow-md"
+              placeholder="Hospital Id(If any)*"
+            />
+
+          </div>
         </div>
       </div>
 
 
-      <div className="flex md:flex-row flex-2 md:px-5 px-2 py-2 justify-center md:gap-4 gap-1 items-center bg-white w-full overflow-x-auto">
-        <div className="flex flex-col md:gap-3 gap-1">
-          <input type="text" className="border placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px]" placeholder="Name*" />
-          <input type="text" className="border placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px]" placeholder="Appointment Date* " />
-        </div>
-
-        <div className="flex flex-col md:gap-3 gap-1">
-          <input type="text" className="border placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px]" placeholder="Mobile*" />
-          <input type="text" className="border placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px]" placeholder="Appointment Time*" />
-        </div>
-
-        <div className="flex flex-col md:gap-3 gap-1">
-          <input type="text" className="border placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px] " placeholder="Email*" />
-          <input
-            type="text"
-            className="border placeholder-gray-500 py-1 md:py-2 px-2 outline-none text-sm w-full placeholder:text-[11px]"
-            placeholder="Hospital Id(If any)*"
-          />
-        </div>
-
-
-      </div>
-      <div className="bg-orange-bg justify-center items-center flex-1 px-2 md:hidden w-full  py-1">
-        <h4 className="text-white text-center text-[10px] sm:text-xs font-poppins">MAKE APPOINMENT</h4>
-      </div>
-
-
-      <div className="bg-orange-bg justify-center items-center flex-1 px-3 hidden md:flex">
-        <h4 className="text-white text-center text-sm font-poppins">MAKE APPOINMENT</h4>
+      <div className="flex justify-center">
+        <button className="bg-dark-green text-white font-poppins px-8 py-2 rounded-lg hover:bg-green text-sm font-light">Submit</button>
       </div>
     </div>
   );
